791a2979f39971ed81c62d79c50befdc6c05a44a
Quincy Cunada <quincyboom@quincys-mbp.lan>
2025-04-10 16:13:17 -0700

    add both exercises

---
+++ practice/src/main/java/ExerciseMathClass.java
@@ -0,0 +1,86 @@
+public class ExerciseMathClass {
+    public static void main (String[] args) {
+
+        int number = -45; //exercise 1
+
+        System.out.println(Math.abs(number));
+
+        double num1 = 3.5; //exercise 2
+        double num2 = 7.8;
+        System.out.println(Math.max(num1, num2));
+
+        int int1 = 99; //exercise 3
+        int int2 = 23;
+
+        System.out.println(Math.min(int1, int2));
+
+        double dbl = 8.9; // exercise 4
+        System.out.println(Math.floor(dbl));
+
+        double dbl1 = 5.2; //exercise 5
+        System.out.println(Math.ceil(dbl1));
+
+        double dbl2 = 9.6; // exercise 6
+        System.out.println(Math.round(dbl2));
+
+        double morningTemp = -3.7;  // exercise 7
+        double afternoonNap = 12.4;
+        System.out.println(Math.abs(morningTemp - afternoonNap));
+        System.out.println(Math.max(morningTemp,afternoonNap));
+        System.out.println(Math.min(morningTemp, afternoonNap));
+        System.out.println(Math.round(afternoonNap));
+        System.out.println(Math.floor(morningTemp));
+
+        double price = 10.876; //bonus 1
+        double roundedNr = (double) Math.round(price * 100) / 100;
+        System.out.println ("bonus 1 = " + roundedNr);
+
+        double duration = 320; //bonus 2
+        System.out.println("real minutes:" + duration / 60);
+        System.out.println("show as:" + Math.ceil(duration/ 60));
+
+        double price1 = 3.87; // bonus 3
+        System.out.println("No pennies : " + Math.round(price1 / 0.05) * 0.05);
+        System.out.println("No pennies: " + (float)Math.floor(price1 / 0.05) * 0.05);
+
+        double x = 5.9; //bonus 4
+        int y = (int)x + 3;
+        System.out.println(y);
+
+        int z = (int)(x + 3);
+        System.out.println(z);
+
+        int a = 17; // bonus 5
+        int b = 42;
+        int c = 9;
+        int maxOfValue = Math.max(a, (Math.max(b,c)));
+        System.out.println(maxOfValue);
+
+        double total = 17.23; // bonus 6
+        int pay = (int) Math.ceil(total);
+        System.out.println("pay euro: " + pay + "euros");
+
+        int x2 = 150; // bonus 7
+        byte y2 = (byte)x2;
+        System.out.println(y2);
+
+        double start = -12.8; //bonus 8
+        double end = 7.3;
+
+        double absDistance = Math.abs(start - end);
+        System.out.println(absDistance);
+        System.out.println(Math.ceil(absDistance));
+        System.out.println((int) absDistance);
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
---
+++ practice/src/main/java/ExerciseOperators.java
@@ -0,0 +1,124 @@
+
+public class ExerciseOperators {
+    public static void main(String[] args) {
+
+        int  nr = 4;
+        double dblNr = 3.2;
+        char letter = 'a';
+
+        System.out.println(nr);      //problem 1
+        System.out.println(dblNr);
+        System.out.println(letter);
+
+        int nrOfTomato = 4;
+        int nrOfApples = 2;
+        System.out.println(nrOfTomato);
+        System.out.println(nrOfApples);
+
+       int sum = (nrOfTomato + nrOfApples);  // problem 2
+        System.out.println("Sum:" + sum);
+
+
+        int result = nrOfTomato / nrOfApples;  // problem 3
+        System.out.println("Result:" + result);
+
+
+        int nr1 = 55;  // problem 4
+        double nr2 = 2.5;
+        System.out.println(nr1 / nr2);
+
+
+        int age = 28; // problem 5
+        System.out.println("In 10 years I will be:" + (age + 10));
+
+
+
+       byte byteNr = 100;  // problem 6
+       int intByteNr = (int) byteNr;
+        System.out.println("Byte Number:" + byteNr);
+        System.out.println("Int Number:" + intByteNr);
+
+
+        double doubleNr = 5.7;  // problem 7
+        int intNr = (int) doubleNr;
+        System.out.println("Double value:" + doubleNr);
+        System.out.println("Int value:" + intNr);
+
+
+        float floatNr = 2.30F;  // problem 8
+        int number = 43;
+        double floatintresult = (floatNr * number);
+        System.out.println("Float times int:" + floatintresult);
+
+
+        char status = 'A';   // problem 9
+        System.out.println("task 9:" + ++status);
+
+
+        int a = 9;  //problem 10
+        int b = 2;
+        System.out.println("Divide a and b:" + a / b);
+        System.out.println("Remainder of a and b:" + a % b);
+
+
+        double weight = 66.0;  //problem 11
+        double height = 1.73;
+        double BMI = weight / (height * height);
+        System.out.println("BMI:" + BMI);
+
+
+        double number1 = 32.0;  // problem 12
+        double number2 = 45.0;
+        double number3 = 11.0;
+        double avg = (number1 + number2 + number3) / 3;
+        System.out.println("Average:" + avg);
+
+        int x = 12;
+
+        System.out.println("initial value:" + x);
+        x += 4;
+
+        int h = (x -= 10);
+        int i = (x *= 2);
+        System.out.println("value after adding 4");
+        System.out.println("value after subtracting 10" +  h);
+        System.out.println("value after multiplying 2" +  i);
+
+        short s = 30000;   // problem 14
+        short newShort = (short)(s * 2);
+        System.out.println(newShort);
+
+        double price = 10.99;  // problem 15
+        int quantity = 5;
+        int totalCost = (int)(price * quantity);
+        System.out.println(totalCost);
+
+
+        char letter1 = 'z'; // problem 17
+        char newLetter = (char) (letter - 2);
+        System.out.println(newLetter);
+
+        long value = 2000000000;  //problem 18
+        long newValue = value + 1000000000;
+        System.out.println(newValue);
+
+        double radius = 4.5;
+        double area = Math.PI * radius * radius;
+        System.out.println(area);
+
+        int totalMinutes = 350;
+        int fullHours = 350 / 60;
+        int remainingMinutes = 350 % 60;
+
+
+
+        double electricityCost = 2.75;
+
+
+
+
+
+
+
+        }
+    }
