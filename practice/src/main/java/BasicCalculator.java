2288a93127a90372d3b6e3d70b40f605229a3858
Quincy Cunada <quincyboom@quincys-mbp.lan>
2025-04-11 11:11:47 -0700

    Added Basic Calculator

---
+++ practice/src/main/java/BasicCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(num1 * num2);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
